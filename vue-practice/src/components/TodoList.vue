c32874daca91298ca3279c76799c0af7b777704d
yu-furukubo <holyn1208@gmail.com>
2025-10-05 16:24:22 +0900

    10/5学習

---
+++ vue-practice/src/components/TodoList.vue
@@ -0,0 +1,25 @@
+<template>
+  <ul>
+    <li v-for="todo in todos" :key="todo.id">
+      <label>
+        <input type="checkbox" :checked="todo.done" @change="$emit('toggle', todo.id)" />
+        <span :style="todo.done ? { textDecoration: 'line-through', opacity: 0.6 } : {}">
+          {{ todo.title }}
+        </span>
+      </label>
+      <button @click="$emit('remove', todo.id)">削除</button>
+    </li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+import { defineProps, defineEmits } from 'vue'
+import type { Todo } from './../App.vue'
+
+defineProps<{ todos: Todo[] }>()
+
+defineEmits<{
+  (e: 'toggle', id: number): void
+  (e: 'remove', id: number): void
+}>()
+</script>
