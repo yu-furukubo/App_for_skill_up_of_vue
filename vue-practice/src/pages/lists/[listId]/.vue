ed7cc3ef930bda4bd84b0140dd1253d706cdf4b5
yu-furukubo <holyn1208@gmail.com>
2025-10-18 20:58:25 +0900

    一時保存

---
+++ vue-practice/src/pages/lists/[listId]/.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>{{ store.lists[listId]?.name }}</h2>
+    <nav class="mb-2">
+      <RouterLink :to="`/lists/${listId}`">Todos</RouterLink> |
+      <RouterLink :to="`/lists/${listId}/settings`">Settings</RouterLink>
+    </nav>
+    <RouterView :key="$route.fullPath" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+import { useMultiTodoStore } from '@/stores/multiTodo'
+
+const route = useRoute()
+const store = useMultiTodoStore()
+const listId = computed(() => Number(route.params.listId))
+</script>
